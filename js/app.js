eeb924aa93198d2b371975c76278f6438e2d4291
Oklein1 <ofirklein2@gmail.com>
2021-12-15 12:25:01 -0500

    organized files: sep out js/css/html

---
+++ js/app.js
@@ -0,0 +1,31 @@
+const map = L.map("iiif-map", {
+    center: [0, 0],
+    crs: L.CRS.Simple,
+    zoom: 0,
+});
+
+
+
+// ALTERNATIVE MAP URL: https://www.digitalcommonwealth.org/search/commonwealth:3f462s82t
+const endNode = imgJson["sequences"][0]["canvases"][0]["images"][0]["resource"]["service"]["@id"]
+
+const jerusalemLayer = new L.tileLayer.iiif(endNode + "/info.json").addTo(map);
+
+
+map.on("moveend", () => {
+    // returns 'southwest_lng,southwest_lat,northeast_lng,northeast_lat' of current view
+    const currentBounds = map.getBounds().toBBoxString();
+    //console.log(currentBounds)
+
+    // output current bounds to screen
+    document.getElementById("current-bounds").innerText = currentBounds;
+});
+
+document
+    .getElementById("zoom-to-legend")
+    .addEventListener("click", () => {
+        map.flyToBounds([
+            [-584, 12],
+            [-284, 312],
+        ]);
+    });
