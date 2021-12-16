1f9b2d228114d52fe8f5a4d3fd9f855f1218a118
Oklein1 <ofirklein2@gmail.com>
2021-12-16 16:58:17 -0500

    basic structure and functionality refactored

--- js/app.js
+++ js/app.js
@@ -9,63 +9,14 @@ const endNode = imgJson["sequences"][0]["canvases"][0]["images"][0]["resource"][
 
 const mercatorMap = new L.tileLayer.iiif(endNode + "/info.json").addTo(map);
 
-// split to get this to like one line
 
 document.getElementById('ui').addEventListener('click', e => {
-    const target = e.target;
-    console.log(e)
-    if (target.id === "start") {
-        document.getElementById("text-box").innerText = pagesJson["start"]["description"]; 
-        map.flyToBounds([pagesJson["start"]["mapZoom"]]);
-        
-    } 
-    else if (target.id === "scene_1") {
-        document.getElementById("text-box").innerText = pagesJson["scene1"]["description"]; 
-        map.flyToBounds([pagesJson["scene1"]["mapZoom"]]);
-        
-    }
-    else if (target.id === "scene-two") {
-        document.getElementById("text-box").innerText = pagesJson["scene2"]["description"]; 
-        map.flyToBounds([pagesJson["scene2"]["mapZoom"]]);
-
-    } else if (target.id == "scene-three"){
-        document.getElementById("text-box").innerText = pagesJson["scene3"]["description"]; 
-        map.flyToBounds([pagesJson["scene3"]["mapZoom"]]);
-    }
-    else if (target.id == "scene-four"){
-        document.getElementById("text-box").innerText = pagesJson["scene4"]["description"]; 
-        map.flyToBounds([pagesJson["scene4"]["mapZoom"]]);
-    }
-    else if (target.id == "scene-four-continued"){
-        document.getElementById("text-box").innerText = pagesJson["scene4Continued"]["description"]; 
-        map.flyToBounds([pagesJson["scene4Continued"]["mapZoom"]]);
-    }
-    else if (target.id == "scene-five"){
-        document.getElementById("text-box").innerText = pagesJson["scene5"]["description"]; 
-        map.flyToBounds([pagesJson["scene5"]["mapZoom"]]);
-    }
-    else if (target.id == "scene-six"){
-        document.getElementById("text-box").innerText = pagesJson["scene6"]["description"]; 
-        map.flyToBounds([pagesJson["scene6"]["mapZoom"]]);
-    }
-    else if (target.id == "scene-seven"){
-        document.getElementById("text-box").innerText = pagesJson["scene7"]["description"]; 
-        map.flyToBounds([pagesJson["scene7"]["mapZoom"]]);
-    }
-    else if (target.id == "scene-eight"){
-        document.getElementById("text-box").innerText = pagesJson["scene8"]["description"]; 
-        map.flyToBounds([pagesJson["scene8"]["mapZoom"]]);
-    }
-    else if (target.id == "scene-eight-continued"){
-        document.getElementById("text-box").innerText = pagesJson["scene8Continued"]["description"]; 
-        map.flyToBounds([pagesJson["scene8Continued"]["mapZoom"]]);
-    }
-    else if (target.id == "end"){
-        document.getElementById("text-box").innerText = pagesJson["end"]["description"]; 
-        map.flyToBounds([pagesJson["end"]["mapZoom"]]);
+    const id = e.target.id
+    if (id) {
+        document.getElementById("text-box").innerText = pagesJson[[id]]["description"];
+        map.flyToBounds([pagesJson[[id]]["mapZoom"]]);
     }
 }, false);
-
 
 
 // get bounds
@@ -78,4 +29,3 @@ map.on("moveend", () => {
     document.getElementById("current-bounds").innerText = currentBounds;
     //document.getElementById("text-box").innerText = "Test"; // place this anywhere extactly as it is
 });
-
