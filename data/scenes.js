f78687e6c92c1db3a61324a84b983c476c787912
Oklein1 <ofirklein2@gmail.com>
2021-12-17 19:00:53 -0500

    helo-geo centric starting point

---
+++ data/scenes.js
@@ -0,0 +1,61 @@
+// was ist die Frage?
+
+const data = {
+    scene0: {
+        location: 0,
+        description: "Maps are fascinating artifacts not only in what they visualize, but more so in the presumptions they disclose. For contemporary map-makers we deal with arithmetic collection of data representing points along a raster grid. We perform arithmetic transformations on such data in order to visualize them spherically. But this was not always the case, for points were not derived from storage in memory, but from the stars. Let us look a bit closer into this matter.",
+        mapZoom: [
+            [-697.5255432128906,-390.13043212890625],
+            [66.47445678710938,1049.8695678710938]
+        ]
+    },
+    scene1: {
+        location: 1,
+        description: 
+        "In the mid-17th century, Andreas Cellarius, a Dutch mathematician and geographer, compiled a lavish celestial atlas. The Harmonia Macrocosmica covered 1500 years of astronomical theory. It begins with Ptolemy’s Almagest star catalog from 150 A.D., the oldest surviving comprehensive document of celestial motion. 'The Macrocosmica' then describes the renaissance theories of Nicolaus Copernicus, who’s 1543 book On the Revolutions of the Celestial Spheres made the dramatic statement that the earth orbited the sun, and became a cornerstone of the scientific revolution. The Macrocosmica continues to catelog Tyco Brahe’s planisphere — a “geo-heliocentric” system where the Sun and Moon orbit the Earth, and the other planets orbit the Sun.",
+        mapZoom: [
+            [-389.69970703125,120.49042129516602],
+            [-198.69970703125,480.490421295166]
+        ]
+    },
+    scene2: {
+        location: 2,
+        description: "",
+        mapZoom: [
+            [-361.1139221191406,158.22722625732422],
+            [20.886077880859375,878.2272262573242,]
+        ]
+    },
+    scene3: {
+        location: 3,
+        description: "",
+        mapZoom: [
+            [-242.748779296875,89.75276947021484],
+            [-51.748779296875,449.75276947021484]
+        ]
+    },
+    scene4: {
+        location: 3,
+        description: "",
+        mapZoom: [
+            [-242.748779296875,89.75276947021484],
+            [-51.748779296875,449.75276947021484]
+        ]
+    },
+    scene5: {
+        location: 3,
+        description: "",
+        mapZoom: [
+            [-242.748779296875,89.75276947021484],
+            [-51.748779296875,449.75276947021484]
+        ]
+    },
+    scene6: {
+        location: 3,
+        description: "",
+        mapZoom: [
+            [-242.748779296875,89.75276947021484],
+            [-51.748779296875,449.75276947021484]
+        ]
+    }
+}
