df794b62636c212d69ca1086f6de25f508d41197
Oklein1 <ofirklein2@gmail.com>
2021-12-17 22:53:31 -0500

    typos fixed

--- data/scenes.js
+++ data/scenes.js
@@ -3,7 +3,7 @@
 const data = {
     scene0: {
         location: 0,
-        description: "Maps are fascinating artifacts not only in what they visualize, but more so in the presumptions they disclose. For contemporary map-makers we deal with arithmetic collection of data representing points along a raster grid. We perform arithmetic transformations on such data in order to visualize them spherically. But this was not always the case, for points were not derived from storage in memory, but from the stars. Let us look a bit closer into this matter from two points of view: functinoality and theology",
+        description: "Maps are fascinating artifacts not only in what they visualize, but more so in the presumptions they disclose. For contemporary map-makers we deal with arithmetic collection of data, representing points along a raster grid. We perform arithmetic transformations on such data in order to visualize them spherically. But this was not always the case, for points were not always derived from storage in computer memory, but from the stars. Let us look a bit closer into this matter from two points of view: functionality and theology",
         mapZoom: [
             [-725.042236328125,-420.7982177734375],
             [38.957763671875,1019.2017822265625]
@@ -12,15 +12,15 @@ const data = {
     scene1: {
         location: 1,
         description: 
-        "In the mid-17th century, Andreas Cellarius, a Dutch mathematician and geographer, compiled a lavish celestial atlas. The Harmonia Macrocosmica covered 1500 years of astronomical theory. It begins with Ptolemy’s Almagest star catalog from 150 A.D., the oldest surviving comprehensive document of celestial motion. 'The Macrocosmica' then describes the renaissance theories of Nicolaus Copernicus, who’s 1543 book On the Revolutions of the Celestial Spheres made the dramatic statement that the earth orbited the sun, and became a cornerstone of the scientific revolution. The Macrocosmica continues to catelog Tyco Brahe’s planisphere — a “geo-heliocentric” system where the Sun and Moon orbit the Earth, and the other planets orbit the Sun.",
+        "In the mid-17th century, Andreas Cellarius, a Dutch mathematician and geographer, compiled a lavish celestial atlas called 'The Harmonia Macrocosmica'; it covered 1500 years of astronomical theory. It begins with Ptolemy’s Almagest star catalog from 150 A.D., the oldest surviving comprehensive document of celestial motion. The Macrocosmica then describes the Renaissance theories of Nicolaus Copernicus, whose 1543 book On the Revolutions of the Celestial Spheres made the dramatic statement that the earth orbited the sun, and became a cornerstone of the scientific revolution. The Macrocosmica continues to catelog Tyco Brahe’s planisphere — a “geo-heliocentric” system where the Sun and Moon orbit the Earth, and the other planets orbit the Sun.",
         mapZoom: [
             [-389.69970703125,120.49042129516602],
             [-198.69970703125,480.490421295166]
         ]
     },
     scene2: {
-        location: 2,
-        description: "Now, before us lies a star clock. On it we notice a number of different elements: the earth at the center, Roman gods encircling it and Zodiac signs bounding the entire heavens. Besides ornamentation, we may be left to ask: is there any functional purpose of this star clock map? Does it impart meaning facts about the world?",
+        location: 2,    
+        description: "Now, before us lies a star clock. On it we notice a number of different elements: the earth at the center, Roman gods encircling it and Zodiac signs bounding the entire heavens. Besides ornamentation, we may be left to ask: how is star-clock map intended to be read? What information about the world does it intend to tell?",
         mapZoom: [
             [-693.4468994140625,-404.8275451660156],
             [70.5531005859375,1035.1724548339844]
@@ -28,7 +28,7 @@ const data = {
     },
     scene3: {
         location: 3,
-        description: "Well, as we know there are 12 Zodiac signs. We can see each one of them on the map. Just as a year is made of up 360 degrees, so too was 1 year (+/- 5 degrees). Therefore, every month denominated 30 degrees, as represented by the blue polygons; it's also worth noting that days and nights varied in time, and so too, generally, did the months. This is why the blue polygons representing the degrees are not exactly even. Now, each blue polygon representing 10 degrees was known as a ‘decan’; a decan was an indicator used to tell when night began; it all depended on the ‘decan’ in the sky.",
+        description: "Well, as we know there are 12 Zodiac signs. We can see each one of them on the map. Just as a circle is made of up 360 degrees, a full year, at that time, was made up of 360 days (+/- 5 days.). Astrological time and geometry were analogous. And the analogy goes further: every month denominated 30 degrees, as represented by the blue polygons; it's also worth noting that days and nights varied in time, and so too, generally, did the months. This is why the blue polygons representing the degrees are not exactly even. Now, each blue polygon representing 10 degrees was known as a ‘decan’; a decan was an indicator used to tell when night began; it all depended on the ‘decan’ in the sky.",
         mapZoom: [
             [-361.1139221191406,158.22722625732422],
             [20.886077880859375,878.2272262573242,]
@@ -36,7 +36,7 @@ const data = {
     },
     scene4: {
         location: 4,
-        description: "As I'm sure many of you are thinking, we are looking a map visualizing the earth at the center of the known universe. The sun, the Deity riding the golden chariot, Solis, encircles us.",
+        description: "As I'm sure many of you are thinking, we are currently looking upon a map visualizing the earth at the center of the known universe. The sun, the Deity riding the golden chariot, Solis, encircles us.",
         mapZoom: [
             [-392.4420700073242,-39.81927490234375],
             [-10.442070007324219,680.1807250976562]
